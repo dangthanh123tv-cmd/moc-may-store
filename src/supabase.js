e32a082a6c7ed57c7791aa03b5dd6da6e29d1654
dangthanh123tv-cmd <dangthanh123tv@gmail.com>
2026-09-22 14:27:47 +0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,12 +1,18 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://deknuzzystcfvbyyboah.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rsqbMvBnAmDJnhJk8iAbxQ_OogptzEJ";
+const SUPABASE_URL = "https://deknuzxystcfvbyyboah.supabase.co";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-  },
-});
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_rsqbMvBnAmDJnhJk8iAbxQ_OogptzEJ";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  }
+);
